e49718598a7f811c0aa88377727502a6abb09c7b
Vall Grice <vallgrice@gmail.com>
2026-04-05 22:40:52 +0300

    Remove unused imports from bottom_nav_shell.dart

--- lib/shared/widgets/bottom_nav_shell.dart
+++ lib/shared/widgets/bottom_nav_shell.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import '../../features/sessions/screens/sessions_screen.dart';
-import '../../features/profile/screens/profile_screen.dart';
 import '../theme/app_colors.dart';
 import '../widgets/app_icon.dart';
 
